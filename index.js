d20c9ff8bc549bdb399e8e4af95e4c48ba6a028a
MhmmdBagci <Muhammed.Bagci@skyered-education.de>
2025-04-28 12:02:59 +0200

    added certificate Python

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // z.B. 'linux': 'certificates/linux_cert.jpg'
 const skillImageMap = {
   "linux & bash": "certificates/linux_bash_cert.jpg",
+  "Python": "certificates/Muhammed Bagci Python.png"
   // Weitere Zuordnungen möglich ...
   // Skills ohne Eintrag erhalten "Zertifikat ist in progress"
 };
