f4ba70f94530415288a288fc27cb3917caa37740
MhmmdBagci <Muhammed.Bagci@skyered-education.de>
2025-04-28 13:04:28 +0200

    updated certificate

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Hier kannst du pro Skill einen Bildpfad hinterlegen,
 // z.B. 'linux': 'certificates/linux_cert.jpg'
 const skillImageMap = {
-  "linux & bash": "certificates/linux_bash_cert.jpg",
-  "python": "certificates/python_cert.jpg"
+  "linux & bash": "./certificates/linux_bash_cert.jpg",
+  "python": "./certificates/python_cert.png"
   // Weitere Zuordnungen möglich ...
   // Skills ohne Eintrag erhalten "Zertifikat ist in progress"
 };
